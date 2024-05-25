cf0a501497d9b8fa871ef9fccd4987050f523ad3
tegaki <drawydraws@protonmail.com>
2024-05-25 20:37:37 +0200

    :sparkles: new component FancyImage

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,12 @@ function App() {
           </Card>
         </CardHolder>
         <FancyImage src="https://http.dog/200.jpg" />
+        <FancyImage src="https://cdn.pixabay.com/photo/2023/09/02/03/15/water-8228076_1280.jpg" />
+        <FancyImage src="https://cdn.pixabay.com/photo/2023/01/15/08/55/dog-7719758_1280.jpg" />
+        <FancyImage src="https://cdn.pixabay.com/photo/2024/05/19/08/39/work-8772016_1280.jpg" />
+        <FancyImage src="https://cdn.pixabay.com/photo/2024/05/19/00/39/cigarette-8771248_1280.png" />
+        <FancyImage src="https://cdn.pixabay.com/photo/2023/09/04/22/35/boy-8233868_1280.png" />
+        <FancyImage src="https://cdn.pixabay.com/photo/2023/06/16/15/14/sunset-8068208_1280.jpg" />
       </main>
     </>
   );
--- src/components/FancyImage.tsx
+++ src/components/FancyImage.tsx
@@ -1,3 +1,6 @@
+import { prominent } from "color.js";
+import { useEffect, useState } from "react";
+
 type Props = {
   src: string;
 };
@@ -7,9 +10,29 @@ type Props = {
  * based on the contents of the image! (shown through padding)
  */
 export const FancyImage = ({ src }: Props) => {
+  const [bgColor, setBgColor] = useState<string | undefined>(undefined);
+  const [imgLoaded, setImgLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const prominent_color = await prominent(src, { amount: 1 });
+      if (Array.isArray(prominent_color)) {
+        console.log(prominent_color);
+        setBgColor(`rgb(${prominent_color.join(",")})`);
+      }
+      console.log("color.js calculate color from image...");
+    })();
+  }, [imgLoaded]);
+
   return (
-    <div className="p-10 bg-pink-800 ">
-      <img src={src} />
+    <div className="p-20 w-fit" style={{ backgroundColor: bgColor }}>
+      <img
+        src={src}
+        className="w-80"
+        onLoad={() => {
+          setImgLoaded(true);
+        }}
+      />
     </div>
   );
 };
