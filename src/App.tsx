a2b468f315dcf5f2d9325298a67b63de56c6ac76
tegaki <drawydraws@protonmail.com>
2024-05-25 16:30:29 +0200

    :art: biome format all

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,51 @@ import { FancyImage } from "./components/FancyImage";
 import { MyComponent } from "./components/MyComponent";
 
 function App() {
-	return (
-		<>
-			<main>
-				<h1>This is the main content!</h1>
-				<MyComponent name="mr. frog" />
-				<Card>
-					<p>this is card is floating on its own</p>
-					<MyComponent name="card person" />
-				</Card>
-				<CardHolder>
-					<Card>
-						<p>
-							this is card is{" "}
-							<span className="bg-gray-800 bg-opacity-10 py-0.5 px-1 rounded-sm">
-								<span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-500">
-									inside
-								</span>
-							</span>{" "}
-							a CardHolder
-						</p>
-						<MyComponent name="card person" />
-					</Card>
-					<Card>
-						<p>
-							this is card is <span className="underline">inside</span> a
-							CardHolder
-						</p>
-						<MyComponent name="card person" />
-					</Card>
-					<Card>
-						<p>
-							this is card is <span className="underline">inside</span> a
-							CardHolder
-						</p>
-						<MyComponent name="card person" />
-					</Card>
-					<Card>
-						<p>
-							this is card is <span className="underline">inside</span> a
-							CardHolder
-						</p>
-						<MyComponent name="card person" />
-					</Card>
-				</CardHolder>
-				<FancyImage src="https://http.dog/200.jpg" />
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main>
+        <h1>This is the main content!</h1>
+        <MyComponent name="mr. frog" />
+        <Card>
+          <p>this is card is floating on its own</p>
+          <MyComponent name="card person" />
+        </Card>
+        <CardHolder>
+          <Card>
+            <p>
+              this is card is{" "}
+              <span className="bg-gray-800 bg-opacity-10 py-0.5 px-1 rounded-sm">
+                <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-500">
+                  inside
+                </span>
+              </span>{" "}
+              a CardHolder
+            </p>
+            <MyComponent name="card person" />
+          </Card>
+          <Card>
+            <p>
+              this is card is <span className="underline">inside</span> a CardHolder
+            </p>
+            <MyComponent name="card person" />
+          </Card>
+          <Card>
+            <p>
+              this is card is <span className="underline">inside</span> a CardHolder
+            </p>
+            <MyComponent name="card person" />
+          </Card>
+          <Card>
+            <p>
+              this is card is <span className="underline">inside</span> a CardHolder
+            </p>
+            <MyComponent name="card person" />
+          </Card>
+        </CardHolder>
+        <FancyImage src="https://http.dog/200.jpg" />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/FancyImage.tsx
+++ src/components/FancyImage.tsx
@@ -1,5 +1,5 @@
 type Props = {
-	src: string;
+  src: string;
 };
 
 /**
@@ -7,9 +7,9 @@ type Props = {
  * based on the contents of the image! (shown through padding)
  */
 export const FancyImage = ({ src }: Props) => {
-	return (
-		<div className="p-10 bg-pink-800 ">
-			<img src={src} />
-		</div>
-	);
+  return (
+    <div className="p-10 bg-pink-800 ">
+      <img src={src} />
+    </div>
+  );
 };
